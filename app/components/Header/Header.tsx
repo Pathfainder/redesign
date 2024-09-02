9423659424d0165d97d12e0823db348eaf440896
Pathfainder <160763159+Pathfainder@users.noreply.github.com>
2024-09-02 13:06:37 +0200

    Update Header.tsx

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -9,11 +9,11 @@ const Header = () => {
 	return (
 		<header className="py-6 flex justify-between">
 			<Link href="..">
-				<h1 className="text-2xl md:text-4xl font-bold">redesigner.io</h1>
+				<h1 className="text-2xl md:text-4xl font-bold">Redesign</h1>
 			</Link>
 			<div className="flex flex-row gap-8">
 				<a
-					href="https://github.com/bgwebagency/redesigner.io"
+					href="https://github.com/Pathfainder/redesign/"
 					target="_blank"
 					rel="noreferrer"
 					className="hover:opacity-50 transition duration-150"
